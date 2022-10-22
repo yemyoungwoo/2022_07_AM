55537e8fb0493ce2ec419d865e607e44264cdcb0
yemyoungwoo <tkddlektks@gmail.com>
2022-10-22 20:18:11 +0900

    SQLErrorException, DBUtil, SecSql, article delete 기능구현

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -229,6 +229,7 @@ public class Main {
 				if (articles.size() == 0) {
 					System.out.println("게시물이 없습니다");
 					continue;
+			
 				}
 				System.out.println("번호    |    제목");
 				for (int i = 0; i < articles.size(); i++) {
