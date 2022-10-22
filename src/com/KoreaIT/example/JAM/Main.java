28413f2a3df2a04d1ddf2249796b7d48e587db7a
yemyoungwoo <tkddlektks@gmail.com>
2022-10-22 19:10:19 +0900

    SQLErrorException, DBUtil, SecSql 추가 및 이를 이용하여 article delete 기능 구현

--- src/com/KoreaIT/example/JAM/Main.java
+++ src/com/KoreaIT/example/JAM/Main.java
@@ -9,8 +9,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.KoreaIT.util.DBUtil;
+import com.KoreaIT.util.SecSql;
+
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("==프로그램 시작==");
 		int lastArticleId = 0;
@@ -28,13 +31,8 @@ public class Main {
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-
 				System.out.printf("%d번글이 생성되었습니다\n", id);
-				System.out.println("title : " + title);
-				System.out.println("body : " + body);
-
 				Article article = new Article(id, title, body);
-
 				Connection conn = null;
 				PreparedStatement pstmt = null;
 				try {
@@ -71,22 +69,60 @@ public class Main {
 						e.printStackTrace();
 					}
 				}
-
 				lastArticleId++;
-
 			} else if (cmd.startsWith("article modify ")) {
-
 				int id = Integer.parseInt(cmd.split(" ")[2]);
-
 				System.out.printf("== %d번 게시물 수정 ==\n", id);
-
 				System.out.printf("새 제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("새 내용 : ");
 				String body = sc.nextLine();
+				Connection conn = null;
+				PreparedStatement pstmt = null;
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/JDBCTest?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+					String sql = "UPDATE article";
+					sql += " SET updateDate = NOW()";
+					sql += ", title = '" + title + "'";
+					sql += ", `body` = '" + body + "'";
+					sql += " WHERE id = " + id + ";";
+					System.out.println(sql);
+					pstmt = conn.prepareStatement(sql);
+					int affectedRows = pstmt.executeUpdate();
+					System.out.println(affectedRows + "열에 적용됨");
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstmt != null && !pstmt.isClosed()) {
+							pstmt.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+				System.out.printf("%d번 게시물이 수정 되었습니다.\n", id);
+
+			} else if (cmd.startsWith("article delete ")) {
+
+				int id = Integer.parseInt(cmd.split(" ")[2]);
+
 
 				Connection conn = null;
 				PreparedStatement pstmt = null;
+				ResultSet rs = null;
 
 				try {
 					Class.forName("com.mysql.jdbc.Driver");
@@ -94,19 +130,27 @@ public class Main {
 
 					conn = DriverManager.getConnection(url, "root", "");
 					System.out.println("연결 성공!");
+					SecSql sql = new SecSql();
+					sql.append("SELECT COUNT(*)");
+					sql.append("FROM article");
+					sql.append("WHERE id = ?", id);
 
-					String sql = "UPDATE article";
-					sql += " SET updateDate = NOW()";
-					sql += ", title = '" + title + "'";
-					sql += ", `body` = '" + body + "'";
-					sql += " WHERE id = " + id + ";";
+					int articlesCount = DBUtil.selectRowIntValue(conn, sql);
 
-					System.out.println(sql);
-					pstmt = conn.prepareStatement(sql);
+					if (articlesCount == 0) {
+						System.out.printf("%d번 게시글은 존재하지 않습니다.\n", id);
+						continue;
+					}
 
-					int affectedRows = pstmt.executeUpdate();
+					System.out.printf("== %d번 게시물 삭제 ==\n", id);
 
-					System.out.println(affectedRows + "열에 적용됨");
+					sql = new SecSql();
+					sql.append("DELETE FROM article");
+					sql.append("WHERE id = ?", id);
+
+					DBUtil.delete(conn, sql);
+
+					System.out.printf("%d번 게시물이 삭제 되었습니다\n", id);
 
 				} catch (ClassNotFoundException e) {
 					System.out.println("드라이버 로딩 실패");
@@ -128,12 +172,8 @@ public class Main {
 						e.printStackTrace();
 					}
 				}
-
-				System.out.printf("%d번 게시물이 수정 되었습니다.\n", id);
-
 			} else if (cmd.equals("article list")) {
 				System.out.println("== 게시물 리스트 ==");
-
 				Connection conn = null;
 				PreparedStatement pstmt = null;
 				ResultSet rs = null;
